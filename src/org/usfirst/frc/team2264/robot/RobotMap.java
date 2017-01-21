9b1313cf29743d1f2dade08b1f2eb4269ba5ac59
Preeti Pidatala <pidatpre000@isd284.com>
2017-01-20 19:17:57 -0600

    Revert "OI edits"
    This reverts commit 806c5bade37f452f2df8e5b21511ca65412e1c92.

--- src/org/usfirst/frc/team2264/robot/RobotMap.java
+++ src/org/usfirst/frc/team2264/robot/RobotMap.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team2264.robot;
 
-import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
@@ -18,9 +19,10 @@ public class RobotMap {
     public static final int ropeMotorLeft = 0;
     public static final int ropeMotorRight = 0;
     
-    public static final int rightStick = 0;
-    public static final int leftStick = 1;
-    public static final int gamepad = 2;
+    public static final int rightStickNumber = 0;
+    public static final int leftStickNumber = 1;
+//    public static final int gamepadNumber = 2;
+    public static final int XboxController = 2; 
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
@@ -31,13 +33,19 @@ public class RightStickButtons {
 //	public static final int RZAxis = 2;
 //	public static final int RZRotate =3;
 	
-	public static final int RtopRightButt = 3;
-	public static final int RmidRightButt = 5;
-	public static final int RbottomButt = 1;
-	public static final int RmidLeftButt = 4;
-	public static final int RtopLeftButt = 2;
-	public static final int RbackButt = 0;
-	public static final int RbackBottomButt = 6;
+
+	
+    public static final int RtopRightButtNumber = 4;
+    public static final int RtopMidTopButtNumber = 2;
+    public static final int RtopBottomButtNumber = 1;
+    public static final int RtopLeftButtNumber = 3;
+    public static final int RbackButtNumber = 0;
+    public static final int RbottomLeftBottomButtNumber = 6;
+    public static final int RbottomLeftTopButtNumber = 5;
+    public static final int RbottomRightBottomButtNumber = 9;
+    public static final int RbottomRightTopButtNumber = 10;
+    public static final int RbottomLeftButtNumber = 7;
+    public static final int RbottomRightButtNumber = 8;
 	
 	
 }
@@ -46,21 +54,40 @@ public class LeftStickButtons {
 	public static final int LYAxis = 1;
 //	public static final int LZRotate = 2;
 //	public static final int LSlider = 3;
+	
+    public static final int LtopRightButtNumber = 4;
+    public static final int LtopMidTopButtNumber = 2;
+    public static final int LtopBottomButtNumber = 1;
+    public static final int LtopLeftButtNumber = 3;
+    public static final int LbackButtNumber = 0;
+    public static final int LbottomLeftBottomButtNumber = 6;
+    public static final int LbottomLeftTopButtNumber = 5;
+    public static final int LbottomRightBottomButtNumber = 9;
+    public static final int LbottomRightTopButtNumber = 10;
+    public static final int LbottomLeftButtNumber = 7;
+    public static final int LbottomRightButtNumber = 8;
+	
 }
 // can comment out gamepad if we don't use it
-public class GamepadButtons {
-	public static final int LXAxis = 0;
-	public static final int LYAxis = 1;
-	public static final int LTrigger = 2;
-	public static final int RTrigger = 3;
-	public static final int RXAxis = 4;
-	public static final int RYAxis = 5;
+/** public class GamepadButtons {
+	public static final int LXAxisNumbers = 0;
+	public static final int LYAxisNumber = 1;
+	public static final int LTriggerNumber = 2;
+	public static final int RTriggerNumber = 3;
+	public static final int RXAxisNumber = 4;
+	public static final int RYAxisNumber = 5;
+	
+	public static final int AButtNumber = 0;
+	public static final int BButtNumber = 1;
+	public static final int XButtNumber = 2;
+	public static final int YButtNumber = 3;
+	public static final int 
 	
 	
 	
-	
+**/	
 	
 }
-}
+
 
 
--- src/org/usfirst/frc/team2264/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2264/robot/subsystems/DriveTrain.java
@@ -23,7 +23,7 @@ public class DriveTrain extends Subsystem {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
-    public void takeJoystickInputs(int left, int right){
+    public void takeJoystickInputs(Joystick left, Joystick right){
     	robotDrive2.tankDrive(left,right);
     }
     public void stop(){
